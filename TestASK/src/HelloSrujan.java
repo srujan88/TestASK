177cc5c7dca7ea73166053bf9551bc57b5c6da85
asksr <asksrujank@gmail.com>
2020-08-19 17:32:14 -0500

    Test Reposity for ASK

---
+++ TestASK/src/HelloSrujan.java
@@ -0,0 +1,21 @@
+
+public class HelloSrujan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ageofboy=30;
+		int ageofgirl=20;
+		
+		if ((ageofboy >=25 && ageofgirl >=20))
+		{
+				System.out.println("Get ready for marriage");
+				}
+		else
+		{
+			    System.out.println("Wait for the right age");	
+			    }
+		
+
+	}
+
+}
